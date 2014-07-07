57a67a398cb186221c18b564dc6c03c397ca8635
Ariel Mashraki <ariel@codeoasis.com>
2014-07-07 14:46:39 +0300

    feat(filter.js): create base module

---
+++ src/filters.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name angular.filters
+ * @description
+ *  bunch of useful filters for angularJS
+ */
+
+angular.module('angular.filters', [
+  'ucfirst'
+]);
