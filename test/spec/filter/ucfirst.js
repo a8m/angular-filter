4e04f419b135a57cf60a9821be91cee781837da0
Ariel Mashraki <ariel@codeoasis.com>
2014-07-07 14:49:49 +0300

    test(base):create base + ucfirst filter test

---
+++ test/spec/filter/ucfirst.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('ucfirstFilter', function() {
+
+  var filter;
+
+  beforeEach(module('ucfirst'));
+
+  beforeEach(inject(function($filter) {
+    filter = $filter('ucfirst');
+  }));
+
+  it('should get a string and return it uppercase each first letter', function() {
+    expect(filter('a')).toEqual('A');
+    expect(filter('foo bar baz')).toEqual('Foo Bar Baz');
+    expect(filter('lorem ipsum is simply dummy.... industry.')).toEqual('Lorem Ipsum Is Simply Dummy.... Industry.');
+  });
+
+  it('should get a !string and not touch it', function() {
+    expect(filter({})).toEqual({});
+    expect(filter([])).toEqual([]);
+    expect(filter(1)).toEqual(1);
+    expect(filter(!1)).toBeFalsy();
+  });
+
+});
