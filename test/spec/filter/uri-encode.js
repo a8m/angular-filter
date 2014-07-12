ee46e77f74c55e42149e71268820f378649aeb33
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-12 12:30:15 +0300

    test(uriEncode): created fully test for uriEncode filter

---
+++ test/spec/filter/uri-encode.js
@@ -0,0 +1,42 @@
+'use strict';
+
+describe('uriEncodeFilter', function() {
+
+  var filter,
+      $window;
+
+  beforeEach(module('uri-encode'));
+
+  beforeEach(inject(function($filter, _$window_) {
+    filter = $filter('uriEncode');
+    $window = _$window_;
+  }));
+
+  it('it should get string as parameter and called encodeURI', function() {
+
+    var string = 'foo bar baz';
+
+    spyOn($window, 'encodeURI');
+
+    filter(string);
+
+    expect($window.encodeURI).toHaveBeenCalledWith(string)
+
+  });
+
+  it('should get !string as parameter and return it as is', function() {
+
+    spyOn($window, 'encodeURI');
+
+    expect(filter([])).toEqual([]);
+
+    expect(filter({})).toEqual({});
+
+    expect(filter(777)).toEqual(777);
+
+    expect($window.encodeURI).not.toHaveBeenCalled();
+
+
+  })
+
+});
