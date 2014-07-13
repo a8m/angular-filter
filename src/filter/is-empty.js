d4b89548d5d7d1356553c69d44811dc3878f0826
Ariel Mashraki <ariel@codeoasis.com>
2014-07-13 13:39:18 +0300

    feat(isEmpty.js): create filter

---
+++ src/filter/is-empty.js
@@ -0,0 +1,14 @@
+/**
+ * @ngdoc filter
+ * @name removeSpaces
+ * @kind function
+ *
+ * @description
+ * remove spaces from string, replace with "-" or given argument
+ */
+
+angular.module('is-empty', [])
+  .filter('isEmpty', [function () {
+
+  }
+  ]);
