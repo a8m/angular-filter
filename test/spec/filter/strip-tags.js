5e94b1015b2d74821ab4249e6db12ff853a0332e
Ariel Mashraki <ariel@codeoasis.com>
2014-07-13 16:58:16 +0300

    test(strip-tags.js): add basic test

---
+++ test/spec/filter/strip-tags.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('stripTagsFilter', function () {
+
+  var filter;
+
+  beforeEach(module('strip-tags'));
+
+  beforeEach(inject(function ($filter) {
+    filter = $filter('stripTags');
+  }));
+
+  it('should get a string with tags and splash it', function() {
+    expect(filter('<p>lorem ipsum</p>')).toEqual('lorem ipsum');
+    expect(filter('<div class="block">foo bar</div>')).toEqual('foo bar');
+    expect(filter('<title>awesome title</title>')).toEqual('awesome title');
+  });
+
+  it('should get a !string and not touch it', function() {
+    expect(filter({})).toEqual({});
+    expect(filter([])).toEqual([]);
+    expect(filter(1)).toEqual(1);
+    expect(filter(!1)).toBeFalsy();
+  });
+
+});
