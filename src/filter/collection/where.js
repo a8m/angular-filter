70f3701e4a31224bc5423d52d86b82e7f2c81548
Ariel Mashraki <ariel@codeoasis.com>
2014-07-14 11:04:08 +0300

    feat(where-filter): create where filter

---
+++ src/filter/collection/where.js
@@ -0,0 +1,28 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name where
+ * @kind function
+ *
+ * @description
+ * of each element in a collection to the given properties object,
+ * returning an array of all elements that have equivalent property values.
+ *
+ */
+
+angular.module('where', [])
+  .filter('where', function() {
+    return function (collection, object) {
+
+      if(isUndefined(object)) {
+        return collection;
+      }
+      collection = (isObject(collection)) ?
+        toArray(collection) : collection;
+
+      return collection.filter(function (elm) {
+        return objectContains(object, elm);
+      });
+    }
+  });
