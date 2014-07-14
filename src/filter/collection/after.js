308aeb7b3e2d4c59213a79a60c824579a86800e9
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-14 20:43:32 +0300

    fix(main): register all filter to angular.filter module

--- src/filter/collection/after.js
+++ src/filter/collection/after.js
@@ -1,3 +1,27 @@
+'use strict';
+
 /**
- * Created by arielmashraki on 7/14/14.
+ * @ngdoc filter
+ * @name after
+ * @kind function
+ *
+ * @description
+ * get a collection and specified count, and returns all of the items
+ * in the collection after the specified count.
+ *
  */
+
+angular.module('a8m.after', [])
+    .filter('after', function() {
+      return function (collection, count) {
+
+        collection = (isObject(collection)) ?
+            toArray(collection) :
+            collection;
+
+        return (isArray(collection)) ?
+            collection.slice(count) :
+            collection;
+
+      }
+    });
