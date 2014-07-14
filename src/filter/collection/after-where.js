68ca75da3a70c7b4140ef93fbc7e916377d41caa
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-15 00:40:40 +0300

    feat(after-where): add and create filter after where

---
+++ src/filter/collection/after-where.js
@@ -0,0 +1,31 @@
+'use strict';
+
+/**
+ * @ngdoc filter
+ * @name after-where
+ * @kind function
+ *
+ * @description
+ * get a collection and properties object, and returns all of the items
+ * in the collection after the first that found with the given properties.
+ *
+ */
+
+angular.module('a8m.after-where', [])
+    .filter('afterWhere', function() {
+      return function (collection, object) {
+
+        collection = (isObject(collection)) ?
+            toArray(collection) :
+            collection;
+
+        if(!isArray(collection) || isUndefined(object))
+          return collection;
+
+        var index = collection.map( function( elm ) {
+          return objectContains(object, elm);
+        }).indexOf( true );
+
+        return collection.slice((index === -1) ? 0 : index);
+      }
+    });
