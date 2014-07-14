cb4621e80391e0f880b0f87180aa4bf76438bb96
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-14 23:19:38 +0300

    feat(before): add before filter

--- src/filter/collection/before.js
+++ src/filter/collection/before.js
@@ -1,3 +1,27 @@
+'use strict';
+
 /**
- * Created by arielmashraki on 7/14/14.
+ * @ngdoc filter
+ * @name before
+ * @kind function
+ *
+ * @description
+ * get a collection and specified count, and returns all of the items
+ * in the collection before the specified count.
+ *
  */
+
+angular.module('a8m.before', [])
+    .filter('before', function() {
+      return function (collection, count) {
+
+        collection = (isObject(collection)) ?
+            toArray(collection) :
+            collection;
+
+        return (isArray(collection)) ?
+            collection.slice(0, (!count) ? count : --count) :
+            collection;
+
+      }
+    });
