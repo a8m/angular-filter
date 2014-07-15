683b2a203688fd0d33e3606f75df231f01816ebe
Ariel Mashraki <ariel@codeoasis.com>
2014-07-15 10:07:34 +0300

    feat(stringular):create filter

---
+++ src/filter/string/stringular.js
@@ -0,0 +1,22 @@
+'use strict';
+/**
+ * @ngdoc filter
+ * @name stringular
+ * @kind function
+ *
+ * @description
+ * get string with {\d+} and replace match with enumeration values
+ */
+
+angular.module('a8m.stringular', [])
+  .filter('stringular', function () {
+    return function(input) {
+
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      return input.replace(/{(\d+)}/g, function (match, number) {
+        return isUndefined(args[number]) ? match : args[number];
+      });
+
+    }
+  });
