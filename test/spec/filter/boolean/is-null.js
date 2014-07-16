846b2602e1c718d9eb8790c331fabd31aaf9d14a
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-16 13:29:59 +0300

    test(isNull): create unit test

--- test/spec/filter/boolean/is-null.js
+++ test/spec/filter/boolean/is-null.js
@@ -1,3 +1,26 @@
-/**
- * Created by arielmashraki on 7/16/14.
- */
+'use strict';
+
+describe('isNullFilter', function () {
+
+  var filter;
+
+  beforeEach(module('a8m.is-null'));
+
+  beforeEach(inject(function ($filter) {
+    filter = $filter('isNull');
+  }));
+
+  it('should returns true if the value is null, else false.', function() {
+
+    expect(filter(null)).toBeTruthy();
+
+    expect(filter({})).toBeFalsy();
+    expect(filter([])).toBeFalsy();
+    expect(filter(1)).toBeFalsy();
+    expect(filter(!1)).toBeFalsy();
+    expect(filter('string')).toBeFalsy();
+    expect(filter(undefined)).toBeFalsy();
+
+  });
+
+});
