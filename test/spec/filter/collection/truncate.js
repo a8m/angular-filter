9bb475513465eb79ea94988dec2e525ac0135460
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-16 08:13:44 +0300

    fix(truncate): add more tests

--- test/spec/filter/collection/truncate.js
+++ test/spec/filter/collection/truncate.js
@@ -8,20 +8,36 @@ describe('truncateFilter', function () {
   }));
 
   it('should cut a string if it is longer than the provided length', function () {
+
     expect(filter('lorem ipsum dolor sit amet', 5, '', false)).toEqual('lorem');
     expect(filter('lorem ipsum dolor sit amet', 11, '', false)).toEqual('lorem ipsum');
     expect(filter('lorem ipsum dolor sit amet', 50, '', false)).toEqual('lorem ipsum dolor sit amet');
+
   });
 
   it('should not cut words in the middle if preserve is true', function () {
+
     expect(filter('lorem ipsum dolor sit amet', 7, '', true)).toEqual('lorem ipsum');
     expect(filter('lorem ipsum dolor sit amet', 13, '', true)).toEqual('lorem ipsum dolor');
     expect(filter('lorem ipsum dolor sit amet', 50, '', true)).toEqual('lorem ipsum dolor sit amet');
+
   });
 
   it('should append the provided prefix if a string has been cut', function () {
+
     expect(filter('lorem ipsum dolor sit amet', 7, '...', true)).toEqual('lorem ipsum...');
     expect(filter('lorem ipsum dolor sit amet', 13, '...', true)).toEqual('lorem ipsum dolor...');
     expect(filter('lorem ipsum dolor sit amet', 50, '...', true)).toEqual('lorem ipsum dolor sit amet...');
+
   });
+
+  it('should get !string and return it as-is', function() {
+
+    expect(filter([])).toEqual([]);
+    expect(filter({})).toEqual({});
+    expect(filter(1)).toEqual(1);
+    expect(filter(!1)).toBeFalsy();
+  
+  });
+
 });
