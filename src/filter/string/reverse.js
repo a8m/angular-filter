e5b6790c26832962310bcfdf985babce48daf8ab
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-19 17:32:31 +0300

    feat(reverse): add reverse filter

---
+++ src/filter/string/reverse.js
@@ -0,0 +1,23 @@
+/**
+ * @ngdoc filter
+ * @name reverse
+ * @kind function
+ *
+ * @description
+ * Reverses a string or collection
+ */
+
+angular.module('a8m.reverse', [])
+
+    .filter('reverse',[ function () {
+      return function (input) {
+
+        input = (isObject(input)) ? toArray(input) : input;
+
+        if(isString(input)) {
+          return input.split('').reverse().join('');
+        }
+
+        return (isArray(input)) ? input.reverse() : input;
+      }
+    }]);
