364c7204df90d9b8bd19bd19ddaae197a410c25b
Ariel Mashraki <ariel@codeoasis.com>
2014-07-20 09:46:20 +0300

    feat(removeWith): create remove-with filter

---
+++ src/filter/collection/remove-with.js
@@ -0,0 +1,25 @@
+/**
+ * @ngdoc filter
+ * @name removeWith
+ * @kind function
+ *
+ * @description
+ * get collection and properties object, and removed elements
+ * with this properties
+ */
+
+angular.module('a8m.remove-with', [])
+  .filter('removeWith', function() {
+    return function (collection, object) {
+
+      if(isUndefined(object)) {
+        return collection;
+      }
+      collection = (isObject(collection)) ?
+        toArray(collection) : collection;
+
+      return collection.filter(function (elm) {
+        return !objectContains(object, elm);
+      });
+    }
+  });
