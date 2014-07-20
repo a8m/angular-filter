d514d66ae55de0eea39983c3346f4269c28f0e6c
Ariel Mashraki <ariel@codeoasis.com>
2014-07-20 14:03:23 +0300

    feat(removeFilter): create remove multiple args filter

---
+++ src/filter/collection/remove.js
@@ -0,0 +1,31 @@
+
+/**
+ * @ngdoc filter
+ * @name remove
+ * @kind function
+ *
+ * @description
+ * remove specific members from collection
+ */
+
+angular.module('a8m.remove', [])
+
+  .filter('remove', function () {
+    return function (collection) {
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if(!isArray(collection)) {
+        return collection;
+      }
+
+      return collection.filter( function( member ) {
+        return !args.some(function(nest) {
+          return equals(nest, member);
+        })
+      });
+
+    }
+  });
