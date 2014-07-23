fc9e5b72f99a50f0d12709182ff23f10c716867f
Ariel Mashraki <ariel@codeoasis.com>
2014-07-23 08:49:12 +0300

    feat(searchField): create

---
+++ src/filter/collection/search-field.js
@@ -0,0 +1,37 @@
+
+/**
+ * @ngdoc filter
+ * @name searchField
+ * @kind function
+ *
+ * @description
+ * for each member, join several strings field and add them to
+ * new field called 'searchField' (use for search filtering)
+ */
+
+angular.module('a8m.search-field', [])
+
+  .filter('searchField', ['$parse', function ($parse) {
+    return function (collection) {
+
+      var get, field;
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      var args = Array.prototype.slice.call(arguments, 1);
+
+      if(!isArray(collection) || !args.length) {
+        return collection;
+      }
+
+      return collection.map(function(member) {
+
+        field = args.map(function(field) {
+          get = $parse(field);
+          return get(member);
+        }).join(' ');
+
+        return extend({ searchField: field }, member);
+      });
+    }
+  }]);
