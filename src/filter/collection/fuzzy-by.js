a6365b98fb9151abffadc1825a4ea92ca62d3525
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-25 23:42:49 +0300

    feat(fuzzyByKey): create

--- src/filter/collection/fuzzy-by.js
+++ src/filter/collection/fuzzy-by.js
@@ -1,3 +1,39 @@
 /**
- * Created by arielmashraki on 7/25/14.
+ * @ngdoc filter
+ * @name fuzzyByKey
+ * @kind function
+ *
+ * @description
+ * fuzzy string searching by key
  */
+
+angular.module('a8m.fuzzy-by', [])
+  .filter('fuzzyByKey', ['$parse', function ( $parse ) {
+    return function (collection, property, search, csensitive) {
+
+      var sensitive = csensitive || false,
+        prop, getter;
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(property)) {
+        return collection;
+      }
+
+      getter = $parse(property);
+
+      return collection.filter(function(elm) {
+
+        prop = getter(elm);
+        if(!isString(prop)) {
+          return false;
+        }
+
+        prop = (sensitive) ? prop : prop.toLowerCase();
+        search = (sensitive) ? search : search.toLowerCase();
+
+        return hasApproxPattern(prop, search) !== false
+      })
+    }
+
+ }]);
