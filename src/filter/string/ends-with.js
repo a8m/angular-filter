172fbd472f79205765e6919d8986e3918091ce35
Ariel Mashraki <ariel@codeoasis.com>
2014-07-28 17:10:56 +0300

    feat(endsWith): create endsWith filter

---
+++ src/filter/string/ends-with.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc filter
+ * @name endsWith
+ * @kind function
+ *
+ * @description
+ * checks whether string ends with the starts parameter.
+ */
+
+angular.module('a8m.ends-with', [])
+
+  .filter('endsWith', function () {
+    return function (input, ends, csensitive) {
+
+      var sensitive = csensitive || false,
+        position;
+
+      if(!isString(input) || isUndefined(ends)) {
+        return input;
+      }
+
+      input = (sensitive) ? input : input.toLowerCase();
+      position = input.length - ends.length;
+
+      return input.indexOf((sensitive) ? ends : ends.toLowerCase(), position) !== -1;
+    }
+  });
