08b3cef298788ab258521e8102037cbe0584dab7
Ariel Mashraki <ariel@codeoasis.com>
2014-07-28 16:02:50 +0300

    feat(startWith): create filter

---
+++ src/filter/string/start-with.js
@@ -0,0 +1,25 @@
+/**
+ * @ngdoc filter
+ * @name startWith
+ * @kind function
+ *
+ * @description
+ * checks whether string starts with the starts parameter.
+ */
+
+angular.module('a8m.uri-encode', [])
+
+  .filter('uriEncode', function () {
+    return function (input, start, csensitive) {
+
+      var sensitive = csensitive || false;
+
+      if(!isString(input) || isUndefined(start)) {
+        return input;
+      }
+
+      input = (sensitive) ? input : input.toLowerCase();
+
+      return !input.indexOf((sensitive) ? start : start.toLowerCase());
+    }
+  });
