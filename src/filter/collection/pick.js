ecd9d549712750a3ccecd5354cda7732d5f7b6ef
Ariel Mashraki <ariel@codeoasis.com>
2014-07-29 16:39:30 +0300

    feat(pick): create filter

---
+++ src/filter/collection/pick.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc filter
+ * @name omit
+ * @kind function
+ *
+ * @description
+ * filter collection by expression
+ */
+
+angular.module('a8m.pick', [])
+
+  .filter('pick', ['$parse', function($parse) {
+    return function (collection, expression) {
+
+      collection = (isObject(collection)) ?
+        toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(expression)) {
+        return collection;
+      }
+
+      return collection.filter(function (elm) {
+
+        return $parse(expression)(elm);
+      });
+    }
+  }]);
