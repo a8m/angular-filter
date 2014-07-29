697a1f92aff4fe761188edf54be73683b0391046
Ariel Mashraki <ariel@mashraki.co.il>
2014-07-30 00:37:05 +0300

    feat(repeat): create repeat filter

---
+++ src/filter/string/repeat.js
@@ -0,0 +1,37 @@
+/**
+ * @ngdoc filter
+ * @name repeat
+ * @kind function
+ *
+ * @description
+ * Repeats a string n times
+ */
+
+angular.module('a8m.repeat', [])
+
+  .filter('repeat',[ function () {
+    return function (input, n, separator) {
+
+      var times = ~~n;
+
+      if(!isString(input)) {
+        return input;
+      }
+
+      return (!times) ? input : strRepeat(input, --n, separator || '');
+    }
+  }]);
+
+/**
+ * Repeats a string n times with given separator
+ * @param str string to repeat
+ * @param n number of times
+ * @param sep separator
+ * @returns {*}
+ */
+function strRepeat(str, n, sep) {
+  if(!n) {
+    return str;
+  }
+  return str + sep + strRepeat(str, --n, sep);
+}
