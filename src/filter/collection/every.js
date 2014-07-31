4ae696304b2da97d846541588de48ddfb44abf4e
Ariel Mashraki <ariel@codeoasis.com>
2014-07-31 09:19:46 +0300

    feat(every): create every filter

---
+++ src/filter/collection/every.js
@@ -0,0 +1,29 @@
+/**
+ * @ngdoc filter
+ * @name every
+ * @kind function
+ *
+ * @description
+ * Checks if given expression is present in all members in the collection
+ *
+ */
+
+angular.module('a8m.every', [])
+  .filter('every', ['$parse', function($parse) {
+    return function (collection, expression) {
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(expression)) {
+        return true;
+      }
+
+      return collection.every( function(elm) {
+
+        return (isObject(elm) || isFunction(expression)) ?
+          $parse(expression)(elm) :
+          elm === expression;
+      });
+
+    }
+  }]);
