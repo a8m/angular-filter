7e0dc3c8fb4952ccc33c9fdd86f873c62d48d04e
Ariel Mashraki <ariel@codeoasis.com>
2014-07-31 08:09:30 +0300

    feat(containsFilter): create

---
+++ src/filter/collection/contains.js
@@ -0,0 +1,33 @@
+/**
+ * @ngdoc filter
+ * @name contains
+ * @kind function
+ *
+ * @description
+ * Checks if given expression is present in one or more object in the collection
+ */
+
+angular.module('a8m.contains', [])
+  .filter({
+    contains: ['$parse', containsFilter],
+    some: ['$parse', containsFilter]
+  });
+
+function containsFilter( $parse ) {
+    return function (collection, expression) {
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(expression)) {
+        return collection;
+      }
+
+      return collection.some( function(elm, index, self) {
+
+        return (isObject(elm)) ? $parse(expression)(elm) :
+          self.indexOf(expression);
+
+      });
+
+    }
+ }
