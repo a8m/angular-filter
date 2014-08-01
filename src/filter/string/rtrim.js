114a04831188e60e316e37c3aea0388ab5c4e641
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-01 22:38:26 +0300

    fix(rtrim): fix regex for filter

--- src/filter/string/rtrim.js
+++ src/filter/string/rtrim.js
@@ -1,3 +1,23 @@
 /**
- * Created by arielmashraki on 8/1/14.
- */
+* @ngdoc filter
+* @name rtrim
+* @kind function
+*
+* @description
+* Right trim. Similar to trimFilter, but only for right side.
+*/
+
+angular.module('a8m.rtrim', [])
+
+  .filter('rtrim', function () {
+    return function(input, chars) {
+
+      var trim = chars || '\\s';
+
+      if(!isString(input)) {
+        return input;
+      }
+
+      return input.replace(new RegExp(trim + '+$'), '');
+    }
+  });
