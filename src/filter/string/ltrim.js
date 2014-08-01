e6c633b1bb7a9a0ec4d1eab116958ec813956f3f
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-01 20:45:33 +0300

    fix(ltrim): fix regex for ltrim

--- src/filter/string/ltrim.js
+++ src/filter/string/ltrim.js
@@ -1,3 +1,23 @@
 /**
- * Created by arielmashraki on 8/1/14.
+ * @ngdoc filter
+ * @name ltrim
+ * @kind function
+ *
+ * @description
+ * Left trim. Similar to trimFilter, but only for left side.
  */
+
+angular.module('a8m.ltrim', [])
+
+  .filter('ltrim', function () {
+    return function(input, chars) {
+
+      var trim = chars || '\\s';
+
+      if(!isString(input)) {
+        return input;
+      }
+
+      return input.replace(new RegExp('^' + trim + '+', 'g'), '');
+    }
+  });
