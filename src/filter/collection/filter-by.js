0834f22da8c7713e3968fff839ef26f560578300
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-02 13:42:43 +0300

    fix(filter-by): use some function for better premformence

--- src/filter/collection/filter-by.js
+++ src/filter/collection/filter-by.js
@@ -1,3 +1,38 @@
 /**
- * Created by arielmashraki on 8/2/14.
+ * @ngdoc filter
+ * @name filterBy
+ * @kind function
+ *
+ * @description
+ * filter by specific properties, avoid the rest
  */
+angular.module('a8m.filter-by')
+
+  .filter('filterBy', ['$parse', function( $parse ) {
+    return function(collection, properties, search) {
+
+      var comparator;
+
+      search = (isString(search)) ? search.toLowerCase() : undefined;
+
+      collection = (isObject(collection)) ? toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(search)) {
+        return collection;
+      }
+
+      return collection.filter(function(elm) {
+
+        return properties.some( function(prop) {
+
+          comparator  = $parse(prop)(elm);
+
+          return (isString(comparator)) ?
+            !comparator.toLowerCase().indexOf(search) :
+            false;
+        })
+
+      });
+
+    }
+  }]);
