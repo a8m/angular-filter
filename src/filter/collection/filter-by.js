376aa9144bb592a5b9ccb0a358b6003943d636ac
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-02 14:38:09 +0300

    feat(filterBy): add regex for concatenate properties

--- src/filter/collection/filter-by.js
+++ src/filter/collection/filter-by.js
@@ -23,9 +23,23 @@ angular.module('a8m.filter-by')
 
       return collection.filter(function(elm) {
 
-        return properties.some( function(prop) {
+        return properties.some(function(prop) {
 
-          comparator  = $parse(prop)(elm);
+          /**
+           * check if there is concatenate properties
+           * example:
+           * object: { first: 'foo', last:'bar' }
+           * filterBy: ['first + last'] => search by full name(i.e 'foo bar')
+           */
+          if(!~prop.indexOf('+')) {
+            comparator = $parse(prop)(elm)
+          }else {
+            var propList = prop.replace(new RegExp('\\s', 'g'), '').split('+');
+            comparator = propList.reduce(function(prev, cur, index) {
+              return (index === 1) ? $parse(prev)(elm) + ' ' + $parse(cur)(elm) :
+                prev + ' ' + $parse(cur)(elm);
+            });
+          }
 
           return (isString(comparator)) ?
             !comparator.toLowerCase().indexOf(search) :
