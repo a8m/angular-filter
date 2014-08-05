afa79193a3e81f17014a4e6412cc4ac975ecefb0
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-05 23:58:18 +0300

    fix(truncate): fix issue #6

--- test/spec/filter/string/truncate.js
+++ test/spec/filter/string/truncate.js
@@ -13,6 +13,9 @@ describe('truncateFilter', function () {
     expect(filter('lorem ipsum dolor sit amet', 11, '', false)).toEqual('lorem ipsum');
     expect(filter('lorem ipsum dolor sit amet', 50, '', false)).toEqual('lorem ipsum dolor sit amet');
 
+    expect(filter('abcdef', 3, '', false)).toEqual('abc');
+    expect(filter('abcd ef', 6, '', false)).toEqual('abcd e');
+
   });
 
   it('should not cut words in the middle if preserve is true', function () {
@@ -21,13 +24,16 @@ describe('truncateFilter', function () {
     expect(filter('lorem ipsum dolor sit amet', 13, '', true)).toEqual('lorem ipsum dolor');
     expect(filter('lorem ipsum dolor sit amet', 50, '', true)).toEqual('lorem ipsum dolor sit amet');
 
+    expect(filter('abcdef', 3, '', true)).toEqual('abcdef');
+    expect(filter('abcd ef', 6, '', true)).toEqual('abcd ef');
+
   });
 
   it('should append the provided prefix if a string has been cut', function () {
 
     expect(filter('lorem ipsum dolor sit amet', 7, '...', true)).toEqual('lorem ipsum...');
     expect(filter('lorem ipsum dolor sit amet', 13, '...', true)).toEqual('lorem ipsum dolor...');
-    expect(filter('lorem ipsum dolor sit amet', 50, '...', true)).toEqual('lorem ipsum dolor sit amet...');
+    expect(filter('lorem ipsum dolor sit amet', 50, '...', true)).toEqual('lorem ipsum dolor sit amet');
 
   });
 
