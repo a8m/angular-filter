9d92974df1e555b542820fdfc4a2dbeef5dbef55
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-06 21:26:23 +0300

    fix(xor): xor support expression

--- src/filter/collection/xor.js
+++ src/filter/collection/xor.js
@@ -1,3 +1,35 @@
 /**
- * Created by arielmashraki on 8/6/14.
+ * @ngdoc filter
+ * @name xor
+ * @kind function
+ *
+ * @description
+ * Exclusive or filter by expression
  */
+
+angular.module('a8m.xor', [])
+
+  .filter('xor', ['$parse', function($parse) {
+    return function (col1, col2, expression) {
+
+      expression = expression || false;
+
+      col1 = (isObject(col1)) ? toArray(col1) : col1;
+      col2 = (isObject(col2)) ? toArray(col2) : col2;
+
+      if(!isArray(col1) || !isArray(col2)) return col1;
+
+      return col1.filter(function(sElm) {
+
+        return !col2.some(function(dElm) {
+
+          var getter = $parse(expression);
+
+          return (expression) ?
+            equals(getter(sElm), getter(dElm)) :
+            equals(sElm, dElm);
+        })
+      });
+
+    }
+  }]);
