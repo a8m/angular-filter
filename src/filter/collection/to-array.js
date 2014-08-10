3e0bf97bb8991225ad90e98b29f9e00fa2a5816d
Ariel Mashraki <ariel@codeoasis.com>
2014-08-10 08:00:55 +0300

    feat(toArray): create

---
+++ src/filter/collection/to-array.js
@@ -0,0 +1,29 @@
+/**
+ * @ngdoc filter
+ * @name toArray
+ * @kind function
+ *
+ * @description
+ * Convert objects into stable arrays.
+ * if addKey set to true,the filter also attaches a new property
+ * $key to the value containing the original key that was used in
+ * the object we are iterating over to reference the property
+ */
+
+angular.module('a8m.to-array', [])
+
+  .filter('toArray', function() {
+    return function (collection, addKey) {
+
+      if(!isObject(collection)) {
+        return collection;
+      }
+
+      return (!addKey) ? toArray(collection) :
+
+        Object.keys(collection).map(function(key) {
+          return Object.defineProperty(collection[key], '$key', { enumerable: false, value: key });
+        });
+
+    }
+  });
