10f4afd3f66dd761cb0421fc3f48f252f74c5ca5
Ariel Mashraki <ariel@codeoasis.com>
2014-08-11 07:40:48 +0300

    feat(map): create map filter

---
+++ src/filter/collection/map.js
@@ -0,0 +1,27 @@
+/**
+ * @ngdoc filter
+ * @name map
+ * @kind function
+ *
+ * @description
+ * Returns a new collection of the results of each expression execution.
+ */
+
+angular.module('a8m.map', [])
+
+  .filter('map', ['$parse', function($parse) {
+    return function (collection, expression) {
+
+      collection = (isObject(collection)) ?
+        toArray(collection) : collection;
+
+      if(!isArray(collection) || isUndefined(expression)) {
+        return collection;
+      }
+
+      return collection.map(function (elm) {
+
+        return $parse(expression)(elm);
+      });
+    }
+  }]);
