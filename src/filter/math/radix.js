ef4e3673f22b51f512af073cc66045eb74d2710b
Ariel Mashraki <ariel@codeoasis.com>
2014-08-19 07:29:34 +0300

    refac(radix):

--- src/filter/math/radix.js
+++ src/filter/math/radix.js
@@ -15,10 +15,8 @@ angular.module('a8m.math.radix', [])
 
       var RANGE = /^[1-9]$|^[1-2]\d$|^3[0-6]$/;
 
-      if(!isNumber(input)) return input;
-
-      if(!RANGE.test(radix)) {
-        throw new Error('radix argument must be between 2 and 36');
+      if(!isNumber(input) || !RANGE.test(radix)) {
+        return input;
       }
 
       return input.toString(radix).toUpperCase();
