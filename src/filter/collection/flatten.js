7e67bd58138f0a4bf70bbf749e89286f89465d76
Ariel Mashraki <ariel@codeoasis.com>
2014-08-21 08:28:46 +0300

    fix(flatten): fix flattenFilter, add shallow option

--- src/filter/collection/flatten.js
+++ src/filter/collection/flatten.js
@@ -11,21 +11,36 @@
 angular.module('a8m.flatten', [])
   .filter('flatten', function () {
     return function(collection, shallow) {
-      //WIP, should be mote forgiving, add shallow option,
-      //to be more elegant + test performance
-      return flatten(collection);
+
+      shallow = shallow || false;
+      collection = (isObject(collection)) ? toArray(collection)
+        : collection;
+
+      if(!isArray(collection)) {
+        return collection;
+      }
+
+      return (!shallow) ? flatten(collection, 0) :
+        [].concat.apply([], collection);
     }
   });
 
-
+/**
+ * flatten nested array (the nesting can be to any depth).
+ * @param array {Array}
+ * @param i {int}
+ * @returns {Array}
+ * @private
+ */
 function flatten(array, i) {
   i = i || 0;
-  if(i >= array.length) return array;
-  if(isArray(array[i])) {
-    var temp = flatten(array[i]);
+
+  if(i >= array.length)
+    return array;
+
+  if(Array.isArray(array[i])) {
     return flatten(array.slice(0,i)
-      .concat(temp)
-      .concat(array.slice(i+1)), 0)
+      .concat(array[i], array.slice(i+1)), i);
   }
-  return flatten(array, i+1)
+  return flatten(array, i+1);
 }
