ce08780ba507abae51f78f6e776098e8302b1fad
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-23 20:27:01 +0300

    feat(filterWatcher): create filterWatcherProvider

---
+++ src/provider/watcher.js
@@ -0,0 +1,97 @@
+/**
+ * @ngdoc provider
+ * @name filterWatcher
+ * @kind function
+ *
+ * @description
+ * filterWatchers is a _privateProvider
+ * It's create to solve the problem of $rootScope:infdig(Infinite $digest loop) when using
+ * some filters on the view.
+ */
+
+angular.module('a8m.filter-watcher', [])
+  .provider('filterWatcher', function() {
+
+    var filterPrefix = '_$$';
+
+    /**
+     * @description
+     * change the prefix name for filters on watch phase
+     * @param prefix
+     * @returns {filterWatcher}
+     */
+    this.setPrefix = function(prefix) {
+      filterPrefix = prefix;
+      return this;
+    };
+
+    this.$get = ['$window', function($window) {
+
+      var $$timeout = $window.setTimeout;
+
+      /**
+       * @description
+       * return the filter full name
+       * @param name
+       * @returns {string}
+       * @private
+       */
+      function _getFullName(name) {
+        return filterPrefix + name;
+      }
+
+      /**
+       * @description
+       * return whether or not this object is watched in current phase
+       * @param fName
+       * @param object
+       * @returns {boolean}
+       * @private
+       */
+      function _isWatched(fName, object) {
+        return isDefined(object[fName]);
+      }
+
+      /**
+       * @description
+       * return the object.$$filterName instance in current phase
+       * @param name
+       * @param object
+       * @private
+       */
+      function _watch(name, object) {
+        var fName = _getFullName(name);
+
+        if(!_isWatched(fName, object)) {
+          //Create new instance
+          Object.defineProperty(object, fName, {
+            enumerable: true,
+            configurable: true,
+            value: {}
+          });
+        }
+        return object[fName];
+      }
+
+      /**
+       * @description
+       * destroy/delete current watch instance
+       * @param name
+       * @param object
+       * @private
+       */
+      function _destroy(name, object) {
+        $$timeout(function() {
+          delete object[_getFullName(name)];
+        });
+      }
+
+      return {
+        $watch: _watch,
+        $destroy: _destroy
+      }
+
+
+    }];
+
+  });
