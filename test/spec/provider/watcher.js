1a291e6d3608119fba8d08802cad2499604abd43
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-24 07:43:37 +0300

    fix(travis): fix broken build

--- test/spec/provider/watcher.js
+++ test/spec/provider/watcher.js
@@ -19,41 +19,37 @@ describe('filterWatcherProvider', function() {
     });
   }));
 
-//  it('should register watcher and return new object', inject(function(filterWatcher) {
-//    var watched = {},
-//      flag = filterWatcher.$watch('foo', watched);
-//
-//    expect(flag).toEqual({});
-//    expect(watched).toEqual(jasmine.objectContaining({
-//      _$$foo : {}
-//    }));
-//  }));
-//
-//  it('should return the same watcher instance if already exist', inject(function(filterWatcher) {
-//    var watched = { _$$bar: [ 1, 2, 3] },
-//      flag = filterWatcher.$watch('bar', watched);
-//
-//    expect(flag).toEqual([ 1, 2, 3]);
-//  }));
-//
-//  it('should be able to remove specific watcher instance', inject(function(filterWatcher) {
-//    var watched = { _$$bar: [ 1, 2, 3] };
-//    filterWatcher.$destroy('bar', watched);
-//
-//    expect(watched).toEqual({});
-//  }));
-//
-//  it('should be able to change the watcherPrefix', function() {
-//    module(setPrefix('a8m_'));
-//    inject(function(filterWatcher) {
-//      var watched = {},
-//        flag = filterWatcher.$watch('foo', watched);
-//
-//      expect(flag).toEqual({});
-//      expect(watched).toEqual(jasmine.objectContaining({
-//        a8m_foo : {}
-//      }));
-//    });
-//  });
+  it('should register watcher and return new object', inject(function(filterWatcher) {
+    var watched = {},
+      flag = filterWatcher.$watch('foo', watched);
+
+    expect(flag).toEqual({});
+    expect(watched).toEqual( {_$$foo: {} } );
+  }));
+
+  it('should return the same watcher instance if already exist', inject(function(filterWatcher) {
+    var watched = { _$$bar: [ 1, 2, 3] },
+      flag = filterWatcher.$watch('bar', watched);
+
+    expect(flag).toEqual([ 1, 2, 3]);
+  }));
+
+  it('should be able to remove specific watcher instance', inject(function(filterWatcher) {
+    var watched = { _$$bar: [ 1, 2, 3] };
+    filterWatcher.$destroy('bar', watched);
+
+    expect(watched).toEqual({});
+  }));
+
+  it('should be able to change the watcherPrefix', function() {
+    module(setPrefix('a8m_'));
+    inject(function(filterWatcher) {
+      var watched = {},
+        flag = filterWatcher.$watch('foo', watched);
+
+      expect(flag).toEqual({});
+      expect(watched).toEqual({ a8m_foo : {} });
+    });
+  });
 
 });
