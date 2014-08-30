8fd74807fe7ee1c5fbd6f2acea29d8db28af0a84
Ariel Mashraki <ariel@mashraki.co.il>
2014-08-31 00:33:25 +0300

    feat(sum): create sum filter

---
+++ src/_filter/math/sum.js
@@ -0,0 +1,23 @@
+/**
+ * @ngdoc filter
+ * @name sum
+ * @kind function
+ *
+ * @description
+ * Sum up all values within an array
+ *
+ */
+
+angular.module('a8m.math.sum', [])
+
+  .filter('sum', function () {
+    return function (input, initial) {
+
+      return (!isArray(input)) ? input :
+        input.reduce(function(prev, curr) {
+          return prev + curr;
+        }, initial || 0);
+
+    }
+
+  });
