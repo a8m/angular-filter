bb50eabb196779431741ed738fe41cc64ee00205
benbakhar <benbakhar@gmail.com>
2014-10-11 22:32:51 +0300

    feat(): added average filter to math filters

---
+++ src/_filter/math/average.js
@@ -0,0 +1,28 @@
+/**
+ * @ngdoc filter
+ * @name average
+ * @kind function
+ *
+ * @description
+ * calculate average number of given array
+ * TODO calculate average of an attribute of given object
+ */
+
+angular.module('a8m.math.average', ['a8m.math'])
+
+  .filter('average', function () {
+    return function (input) {
+    	
+    	if(!isArray(input)) {
+    		return input
+    	}
+    	var res = 0;
+		input.forEach(function(num) {
+			res += num;
+		});
+		return res/input.length
+	}
+	});
+
+ 
+ 
