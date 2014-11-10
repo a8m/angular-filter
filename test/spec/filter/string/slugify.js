54d5f1c64cee4d46430c03ca0c9a651176c446ac
Patricia Lor <patricia.lor@gmail.com>
2014-11-10 23:32:23 +1100

    Typo fix.

--- test/spec/filter/string/slugify.js
+++ test/spec/filter/string/slugify.js
@@ -16,7 +16,7 @@ describe('slugifyFilter', function () {
     expect(filter('Lorem ipsum dolor sit amet')).toEqual('lorem-ipsum-dolor-sit-amet');
   });
 
-  it('should get a string with replacer and replace spaces withit', function() {
+  it('should get a string with replacer and replace spaces with it', function() {
     expect(filter('a a', 1)).toEqual('a1a');
     expect(filter('foo bar baz', '!')).toEqual('foo!bar!baz');
     expect(filter('lorem ipsum dolor sit amet', ' ')).toEqual('lorem ipsum dolor sit amet');
