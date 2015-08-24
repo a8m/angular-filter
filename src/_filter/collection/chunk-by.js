60fc74ac988f3d07d0f82a9bbeea23c5c0a8a902
sbuhr <sonke.buhr@kabelmail.de>
2015-08-24 11:04:20 +0200

    Prevent chunkBy to produce infinite digest loop.

--- src/_filter/collection/chunk-by.js
+++ src/_filter/collection/chunk-by.js
@@ -7,29 +7,35 @@
  * Collect data into fixed-length chunks or blocks
  */
 
-angular.module('a8m.chunk-by', [])
-  .filter('chunkBy', [function () {
-    /**
-     * @description
-     * Get array with size `n` in `val` inside it.
-     * @param n
-     * @param val
-     * @returns {Array}
-     */
-    function fill(n, val) {
-      var ret = [];
-      while(n--) ret[n] = val;
-      return ret;
-    }
+angular.module('a8m.chunk-by', ['a8m.filter-watcher'])
+    .filter('chunkBy', ['filterWatcher', function (filterWatcher) {
+      return function (array, n, fillVal) {
 
-    return function (array, n, fillVal) {
-      if (!isArray(array)) return array;
-      return array.map(function(el, i, self) {
-        i = i * n;
-        el = self.slice(i, i + n);
-        return !isUndefined(fillVal) && el.length < n
-          ? el.concat(fill(n - el.length, fillVal))
-          : el;
-      }).slice(0, Math.ceil(array.length / n));
-    }
-  }]);
+        return filterWatcher.isMemoized('chunkBy', arguments) ||
+            filterWatcher.memoize('chunkBy', arguments, this,
+                _chunkBy(array, n, fillVal));
+        /**
+         * @description
+         * Get array with size `n` in `val` inside it.
+         * @param n
+         * @param val
+         * @returns {Array}
+         */
+        function fill(n, val) {
+          var ret = [];
+          while (n--) ret[n] = val;
+          return ret;
+        }
+
+        function _chunkBy(array, n, fillVal) {
+          if (!isArray(array)) return array;
+          return array.map(function (el, i, self) {
+            i = i * n;
+            el = self.slice(i, i + n);
+            return !isUndefined(fillVal) && el.length < n
+                ? el.concat(fill(n - el.length, fillVal))
+                : el;
+          }).slice(0, Math.ceil(array.length / n));
+        }
+      }
+    }]);
