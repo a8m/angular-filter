c435bc6d6eed7885fff720c19299b5ca0eef96c6
Ariel Mashraki <ariel@mashraki.co.il>
2015-11-17 09:19:16 +0200

    style(test/filter-by): rm unnecessary newlines

--- test/spec/filter/collection/filter-by.js
+++ test/spec/filter/collection/filter-by.js
@@ -1,17 +1,14 @@
 'use strict';
 
 describe('filterByFilter', function() {
-
   var filter;
 
   beforeEach(module('a8m.filter-by'));
-
   beforeEach(inject(function($filter) {
     filter = $filter('filterBy');
   }));
 
   it('should filter by specific properties and avoid the rest', function() {
-
     var users = [
       { id: 1, user: { first_name: 'foo', last_name: 'bar',  mobile: 4444 } },
       { id: 2, user: { first_name: 'bar', last_name: 'foo',  mobile: 3333 } },
@@ -26,11 +23,9 @@ describe('filterByFilter', function() {
     expect(filter(users, ['id', 'user.mobile'], '1')).toEqual([users[0], users[3]]);
     expect(filter(users, ['id'], '1')).toEqual([users[0]]);
     expect(filter(users, ['id', 'user.mobile'], '11')).toEqual([users[3]]);
-
   });
 
   it('should support to get object as collection', function() {
-
     var users = {
       0: { id: 1, user: { first_name: 'foo', last_name: 'bar',  mobile: 4444 } },
       1: { id: 2, user: { first_name: 'bar', last_name: 'foo',  mobile: 3333 } },
@@ -41,11 +36,9 @@ describe('filterByFilter', function() {
     expect(filter(users, ['user.first_name', 'user.last_name'], 'foo')).toEqual(users);
     expect(filter(users, ['user.first_name'], 'oo')).toEqual([users[0], users[2]]);
     expect(filter(users, ['user.last_name'], 'bar')).toEqual([users[0]]);
-
   });
 
   it('should parse concatenate properties, and search them by one field', function() {
-
     var users = [
       { id: 1, user: { first_name: 'foo', last_name: 'bar',  mobile: 4444 } },
       { id: 2, user: { first_name: 'bar', last_name: 'foo',  mobile: 3333 } },
@@ -62,11 +55,9 @@ describe('filterByFilter', function() {
     expect(filter(users, ['user.first_name + user.last_name'], 'a')).toEqual(users);
     expect(filter(users, ['user.first_name + user.last_name'], 'ba')).toEqual(users);
     expect(filter(users, ['user.first_name + user.last_name'], 'foo')).toEqual(users);
-
   });
 
   it('should take care on extreme conditions', function() {
-
     var users = [
       { id: 1, user: { first_name: 'foo', last_name: 'bar',  mobile: 4444 } },
       { id: 2, user: { first_name: 'bar', last_name: 'foo',  mobile: 3333 } },
@@ -80,16 +71,13 @@ describe('filterByFilter', function() {
     expect(filter(users, ['id', 'phone'], null)).toEqual(users);
     expect(filter(users, null, null)).toEqual(users);
     expect(filter(users, [], [])).toEqual(users);
-
   });
 
   it('should get a !collection and return it as-is', function() {
-
     expect(filter(!1)).toBeFalsy();
     expect(filter(1)).toEqual(1);
     expect(filter('string')).toEqual('string');
     expect(filter(undefined)).toEqual(undefined);
-
   });
 
   it('should not coerce non-string/number properties', function() {
@@ -101,32 +89,23 @@ describe('filterByFilter', function() {
   });
 
   describe('strict mode', function() {
-
     var users = [
       { id: 1, user: { first_name: 'foo', last_name: 'bar',  mobile: 4444 } }
     ];
 
     it('should only return exact matches', function() {
-
       expect(filter(users, ['user.first_name'], 'fo', true)).toEqual([]);
       expect(filter(users, ['user.first_name'], 'foo', true)).toEqual(users);
-
     });
 
     it('should handle multiple properties', function() {
-
       expect(filter(users, ['user.first_name', 'user.last_name'], 'ba', true)).toEqual([]);
       expect(filter(users, ['user.first_name', 'user.last_name'], 'bar', true)).toEqual(users);
-
     });
 
     it('should handle concatenation', function() {
-
       expect(filter(users, ['user.first_name + user.last_name'], 'foo ba', true)).toEqual([]);
       expect(filter(users, ['user.first_name + user.last_name'], 'foo bar', true)).toEqual(users);
-
     });
-
   });
-
 });
